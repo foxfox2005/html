b8be0496edcdae440574414abf041655808a97c9
foxfox2005 <minhhieuanlao1970@gmail.com>
2023-11-12 10:43:51 +0700

    final

---
+++ bai1 js/main.js
@@ -0,0 +1,7 @@
+function lmoews ( ) {
+let ngay = document.getElementById('day') , value
+let thang  = document.getElementById('month') , value
+let nam = document.getElementById('year') 
+let d  = ngay +"-" +thang +"-" +nam
+alert (d)
+}
---
+++ test/main.js
@@ -0,0 +1,2 @@
+let yourName = prompt("nhap ten cua ban")
+document.alert ('xin chao tât'+ yourName)
