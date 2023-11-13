66bf9c4b1b597da35058b3d62988fc6795e0c413
foxfox2005 <minhhieuanlao1970@gmail.com>
2023-11-13 19:42:58 +0700

    final

---
+++ bai tuan 2.1/main.js
@@ -0,0 +1,4 @@
+let day = prompt ('Nhập ngày hiển thị')
+let month = prompt ('Nhập tháng hiển thị')
+let year = prompt ('Nhập năm hiển thị')
+document.write ('kết quả ', day + '-' + month + '-' + year)
