06718cbb9fb73d996ba1c8cda6179e7101f34743
foxfox2005 <minhhieuanlao1970@gmail.com>
2023-11-13 19:17:58 +0700

    final

---
+++ bai 2 thuc hanh/main.js
@@ -0,0 +1,4 @@
+let width = inputWidth = parseInt(prompt('nhap chieu cao'))
+let height = inputHeight = parseInt(prompt('nhap chieu rong'))
+let area = width * height 
+document.write (' dien tich chu nhat la :' + area)
