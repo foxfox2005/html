d33eb73d5db96f49da2bc993753e5df3cedc568e
foxfox2005 <minhhieuanlao1970@gmail.com>
2023-11-13 19:02:36 +0700

    final

---
+++ bai 1 thuc hanh/main.js
@@ -0,0 +1,12 @@
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+
+document.write(i)
+document.write('<br/>')
+document.write(f)
+document.write('<br/>')
+document.write(b)
+document.write('<br/>')
+document.write(s)
