bfd6379121b335f33c3a64f26fe3ad6464fbd5f8
foxfox2005 <minhhieuanlao1970@gmail.com>
2023-12-02 22:26:09 +0700

    final

--- test/main.js
+++ test/main.js
@@ -1,10 +1,1 @@
-let a = prompt ('nhap a')
-let b = prompt ('nhap b')
-let c = prompt ('nhap c')
-if (a > b  && a > c) {
-    document.write (a+' la so lon nhat')
-} else if (b >c) {
-    document.write (b+' la so lon nhat')
-} else {
-    document.write (c+' la so lon nhat')
-}
+let a
